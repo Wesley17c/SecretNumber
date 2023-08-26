1deac521c23b41a8120bbb8f5d5b34a5d904f7f3
wesley soares <ws17c@outlokk.com>
2023-08-26 14:01:24 -0300

    a

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -8,7 +8,7 @@ function gerarNumeroAleatorio(){
     return parseInt(Math.random() * maiorValor + 1);
     
 }
-console.log(numeroSecreto);// estou exibindo o numero secreto no console para fim de testes
+console.log(`o numero secreto é ${numeroSecreto}`);// estou exibindo o numero secreto no console para fim de testes
 
 const elementoMenorValor = document.getElementById('menorValor');
     elementoMenorValor.innerHTML = menorValor;
